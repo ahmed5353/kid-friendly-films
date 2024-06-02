919e38ba8af53202c085126a0a0c0ee47f3bd876
ahmed5353 <ahmedreda5353@gmail.com>
2024-06-02 17:17:30 +0300

    convert local server to remote  server

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,7 +12,7 @@ export interface MovieProps {
 }
 
 function Home() {
-  const { movies } = useMovies();
+  const { movies, isPending } = useMovies();
   const navigate = useNavigate();
   const { setMovie } = useMoviesContext() as MoviesContextValue;
 
@@ -26,20 +26,23 @@ function Home() {
   return (
     <div>
       <h1 className="mb-10 text-2xl font-semibold ">Most Popular</h1>
+      {isPending && <h1 className="text-2xl">Loading...</h1>}
+      {isPending || (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 place-items-center lg:grid-cols-4  xl:grid-cols-5 xl:gap-x-24 gap-y-10">
+          {movies?.map((movie: MovieProps) => (
+            // NOTE: I tried to add more movies but json placeholder wants maxContentLength size of 10000 not exceeded
+            <MovieCard
+              key={movie.id}
+              title={movie.name}
+              description={movie.description}
+              image={movie.image}
+              onClick={() => handleClick(movie)}
+            />
+          ))}
+        </div>
+      )}
 
       {/* Movie Card */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 place-items-center lg:grid-cols-4  xl:grid-cols-5 xl:gap-x-24 gap-y-10">
-        {movies?.map((movie: MovieProps) => (
-          // NOTE: I tried to add more movies but json placeholder wants maxContentLength size of 10000 not exceeded
-          <MovieCard
-            key={movie.id}
-            title={movie.name}
-            description={movie.description}
-            image={movie.image}
-            onClick={() => handleClick(movie)}
-          />
-        ))}
-      </div>
     </div>
   );
 }
--- src/services/moviesApi.ts
+++ src/services/moviesApi.ts
@@ -1,7 +1,10 @@
+const API_URL =
+  "https://my-json-server.typicode.com/ahmed5353/kid-friendly-films/data";
+
 async function getMovies() {
-  const res = await fetch("http://localhost:5173/db.json");
+  const res = await fetch(API_URL);
   const data = await res.json();
-  return data?.data;
+  return data;
 }
 
 export default getMovies;
